09c6aba6ed382694bc9aca559b1c1e5e2529b763
reza <rdaqrbanzadh206@gmail.com>
2024-03-10 10:10:28 -0700

    updated  post

--- resources/js/Components/InputTextarea.vue
+++ resources/js/Components/InputTextarea.vue
@@ -1,45 +1,43 @@
-<script setup xmlns="http://www.w3.org/1999/html">
-import { onMounted, ref } from 'vue';
- const props = defineProps({
+<script setup>
+import {onMounted, ref} from 'vue';
+
+const props = defineProps({
     modelValue: {
         type: String,
         required: true,
     },
     placeholder: String,
-    autoResize:{
-        type:Boolean,
-        default:false
-
+    autoResize: {
+        type: Boolean,
+        default: true
     }
 });
-const emit =defineEmits(['update:modelValue']);
+const emit = defineEmits(['update:modelValue']);
 const input = ref(null);
-
-
 onMounted(() => {
     if (input.value.hasAttribute('autofocus')) {
         input.value.focus();
     }
 });
+defineExpose({focus: () => input.value.focus()});
 
-
-
-
-defineExpose({ focus: () => input.value.focus() });
-
-function onInputChange($event){
-    emit('update:modelValue',$event.target.value)
-
-        if (props.autoResize){
-            input.value.style.height = 'auto';
-            input.value.style.height = input.value.scrollHeight + 'px';
-        }
-
-
-
+function onInputChange($event) {
+    emit('update:modelValue', $event.target.value)
+    adjustHeight()
 }
 
+function adjustHeight() {
+    if (props.autoResize) {
+        input.value.style.height = 'auto';
+        input.value.style.height = input.value.scrollHeight + 'px';
+    }
+}
+
+onMounted(() => {
+    adjustHeight()
+})
 </script>
+
 <template>
     <textarea
         class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
