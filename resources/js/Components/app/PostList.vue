b5721ee47740e0548ad9f1a8ef777de82cef1dbd
reza <rdaqrbanzadh206@gmail.com>
2024-03-12 03:12:59 -0700

    add AttachmentPreviewModal

--- resources/js/Components/app/PostList.vue
+++ resources/js/Components/app/PostList.vue
@@ -3,6 +3,7 @@ import PostItem from "@/Components/app/PostItem.vue";
 import PostModal from "@/Components/app/PostModal.vue";
 import {ref} from "vue";
 import {usePage} from "@inertiajs/vue3";
+import AttachmentPreviewModal from "@/Components/app/AttachmentPreviewModal.vue";
 
 defineProps({
     posts :Array
@@ -10,7 +11,9 @@ defineProps({
 
 const authUser = usePage().props.auth.user;
 const showEditModal = ref(false)
+const showAttachmentsModal = ref(false)
 const editPost = ref({})
+const previewAttachmentsPost = ref({})
 function openEditModal(post) {
     editPost.value = post;
     showEditModal.value = true;
@@ -22,13 +25,25 @@ function onModalHide() {
         user: authUser
     }
 }
+function openAttachmentPreviewModal(post, index) {
+    previewAttachmentsPost.value = {
+        post,
+        index
+    }
+    showAttachmentsModal.value = true;
+}
 </script>
 
 <template>
     <div class="overflow-auto">
-        <PostItem v-for="post of posts" :key="post.id" :post="post" @editClick="openEditModal"/>
-
+        <PostItem v-for="post of posts" :key="post.id" :post="post"
+                  @editClick="openEditModal"
+                  @attachmentClick="openAttachmentPreviewModal"
+        />
         <PostModal :post="editPost" v-model="showEditModal" @hide="onModalHide"/>
+        <AttachmentPreviewModal :attachments="previewAttachmentsPost.post?.attachments || []"
+                                v-model:index="previewAttachmentsPost.index"
+                                v-model="showAttachmentsModal"/>
     </div>
 </template>
 
--- resources/js/Components/app/PostModal.vue
+++ resources/js/Components/app/PostModal.vue
@@ -219,7 +219,6 @@ function undoDelete(myFile){
                                     <button
                                         type="button"
                                         class="flex items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 w-full"
-                                        @click="submit"
                                     >
                                         <BookmarkIcon class="w-4 h-4 mr-2"/>
                                         ارسال
