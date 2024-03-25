94d2b2471bfb95f90dc1ff6d8a27157090aa702c
reza <rdaqrbanzadh206@gmail.com>
2024-03-25 06:35:13 -0700

    fix bug dark mode profile

--- resources/js/Components/app/GroupForm.vue
+++ resources/js/Components/app/GroupForm.vue
@@ -15,7 +15,7 @@ const editorConfig = {
 </script>
 
 <template>
-    <div class="mb-3">
+    <div class="mb-3  dark:text-gray-100">
         <label>Group Name</label>
         <TextInput
             type="text"
@@ -26,14 +26,14 @@ const editorConfig = {
         />
     </div>
 
-    <div class="mb-3">
+    <div class="mb-3  dark:text-gray-100">
         <label>
             <Checkbox name="remember" v-model:checked="form.auto_approval"/>
             Enable Auto Approval
         </label>
     </div>
 
-    <div class="mb-3">
+    <div class="mb-3  dark:text-gray-100">
         <label>About Group</label>
 
         <ckeditor :editor="editor" v-model="form.about" :config="editorConfig"></ckeditor>
--- resources/js/Pages/Group/View.vue
+++ resources/js/Pages/Group/View.vue
@@ -166,7 +166,7 @@ function updateGroup() {
                 >
                     {{ errors.cover }}
                 </div>
-                <div class="group relative bg-white">
+                <div class="group relative bg-white dark:bg-slate-950 dark:text-gray-100">
                     <img :src="coverImageSrc || group.cover_url || '/img/default_cover.jpg'"
                          class="w-full h-[200px] object-cover">
                     <div v-if="isCurrentUserAdmin" class="absolute top-2 right-2 ">
@@ -245,9 +245,9 @@ function updateGroup() {
                     </div>
                 </div>
             </div>
-            <div class="border-t m-4 pt-0">
+            <div class="border-t m-4 mt-0">
                 <TabGroup>
-                    <TabList class="flex bg-white">
+                    <TabList class="flex bg-white dark:bg-slate-950 dark:text-white">
                         <Tab v-slot="{ selected }" as="template">
                             <TabItem text="Posts" :selected="selected"/>
                         </Tab>
@@ -269,7 +269,7 @@ function updateGroup() {
                             <template v-if="posts">
                                 <CreatePost :group="group"/>
                                 <PostList v-if="posts.data.length" :posts="posts.data" class="flex-1"/>
-                                <div v-else class="py-8 text-center">
+                                <div v-else class="py-8 text-center dark:text-gray-100">
                                     There are no posts in this group. Be the first and create it.
                                 </div>
                             </template>
@@ -302,14 +302,14 @@ function updateGroup() {
                                               @approve="approveUser"
                                               @reject="rejectUser"/>
                             </div>
-                            <div class="py-8 text-center">
+                            <div class="py-8 text-center dark:text-gray-100">
                                 There are no pending requests.
                             </div>
                         </TabPanel>
-                        <TabPanel class="bg-white p-3 shadow">
+                        <TabPanel >
                             <TabPhotos :photos="photos" />
                         </TabPanel>
-                        <TabPanel class="bg-white p-3 shadow">
+                        <TabPanel >
                             <template v-if="isCurrentUserAdmin">
                                 <GroupForm :form="aboutForm"/>
                                 <PrimaryButton @click="updateGroup">
